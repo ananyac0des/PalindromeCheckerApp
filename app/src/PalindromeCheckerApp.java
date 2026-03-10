ceba6e8b4ebb44dbc0099dc3aec36f44581f7474
Ananya Pradhan <ananyapradhan06@gmail.com>
2026-03-10 10:49:07 +0530

    Add UC10 Case-Insensitive Space-Ignored Palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,98 +1,29 @@
-class Node {
-    char data;
-    Node next;
-
-    Node(char data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class LinkedListPalindrome {
-
-    Node head;
-
-    // Add node to end of list
-    void append(char data) {
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            head = newNode;
-            return;
-        }
-
-        Node temp = head;
-        while (temp.next != null) {
-            temp = temp.next;
-        }
-
-        temp.next = newNode;
-    }
-
-    // Reverse linked list
-    Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-        Node next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    // Check palindrome
-    boolean isPalindrome() {
-        if (head == null || head.next == null)
-            return true;
-
-        Node slow = head;
-        Node fast = head;
-
-        // Find middle using fast/slow pointer
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        // Reverse second half
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        // Compare both halves
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data)
-                return false;
-
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
-    }
-}
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "A man a plan a canal Panama";
 
-        LinkedListPalindrome list = new LinkedListPalindrome();
+        // Normalize string: remove spaces and convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
 
-        // Convert string to linked list
-        for (char c : input.toCharArray()) {
-            list.append(c);
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = normalized.length() - 1;
+
+        while (start < end) {
+            if (normalized.charAt(start) != normalized.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        if (list.isPalindrome()) {
-            System.out.println(input + " is a Palindrome");
+        if (isPalindrome) {
+            System.out.println("Palindrome");
         } else {
-            System.out.println(input + " is NOT a Palindrome");
+            System.out.println("Not Palindrome");
         }
     }
 }
